6095c0d3ae07e701d549e5a41ad161bb2ec046bc
Compeador <qj6w940q@gmail.com>
2021-06-18 17:52:47 +0800

    complete program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PyQt5.QtCore import Qt, QDateTime
+from PyQt5.QtCore import Qt, QDateTime, QDate
+from PyQt5.QtGui import QTextCursor
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QGridLayout, QMessageBox, QFileDialog,
               QLabel, QLineEdit, QPushButton, QComboBox, QCheckBox, QDateTimeEdit, 
               QTextEdit, QTabWidget, QTableWidget, QTableWidgetItem, QHeaderView)
@@ -6,7 +7,7 @@ import os
 import sys
 import traceback
 import requests
-from datetime import datetime
+from datetime import date, datetime, timedelta
 from bs4 import BeautifulSoup as BS
 from threading import Thread
 
@@ -23,9 +24,19 @@ SigComplete = 1
 SigError    = 2
 SigAbort    = 3
 
+LastError  = None
 MainWindow = None
 MainApp    = None
 Signal     = 0
+
+FastSelectOptions = {
+  '近七天': 7,
+  '這個月': 'ThisMonth',
+  '近 30 日': 30,
+  '近半年': 182.621099,
+  '今年': 'ThisYear',
+  '去年': 'LastYear'
+}
 
 def get_error_logname():
   directory = '.'
@@ -88,6 +99,31 @@ def dump_errorlog(err, errinfo):
   except Exception:
     return False
 
+def str2date(ss, key='-'):
+  try:
+    return datetime(*[int(dat) for dat in ss.split(key)])
+  except Exception:
+    return None
+
+def is_file_writable(filename):
+    try:
+      with open(filename, 'a') as _:
+        pass
+    except Exception:
+      return False
+    return True
+
+def open_external_file(path):
+  try:
+    if sys.platform.startswith('linux'):
+      os.system(f"xdg-open \"{path}\"")  
+    elif sys.platform == 'win32':
+      os.system(f"start /b \"\" \"{path}\"")
+    else:
+      os.system(f"start \"{path}\"")  
+    return True
+  except Exception:
+    return False
 
 ### Main curl process
 def is_internet_available():
@@ -98,20 +134,17 @@ def is_internet_available():
     pass
   return False
 
-def get_page(page):
-  return requests.get(f"{TargetURI}?{PageParam}={page}")
-
-def parse_content(page):
+def parse_content(doc):
   ret = []
-  for post in list(page.select(ContentClassPost)):
+  for post in list(doc.select(ContentClassPost)):
     obj = {}
     # tags
-    try:
-      tags = post.select(ContentClassTags)[0].text
-      _tr = str.maketrans({' ': ''})
-      obj['tags'] = '|'.join([tag for tag in tags.translate(_tr).split('|') if len(tag) > 1])
-    except Exception:
-      pass
+    # try:
+    #   tags = post.select(ContentClassTags)[0].text
+    #   _tr = str.maketrans({' ': ''})
+    #   obj['tags'] = '; '.join([tag for tag in tags.translate(_tr).split('|') if len(tag) > 1])
+    # except Exception:
+    #   pass
     # title & href
     try:
       node = post.select(ContentClassTitle)[0]
@@ -126,6 +159,16 @@ def parse_content(page):
       pass
     if obj:
       ret.append(obj)
+  return ret
+
+def parse_content_tags(doc):
+  ret = set()
+  for node in doc.select('strong'):
+    line = node.text.strip()
+    if len(line) < 2 or not (line[0] == '#' or line[0] == '＃'):
+      continue
+    for tag in line.split():
+      ret.add(tag)
   return ret
 
 ### GUI section
@@ -157,6 +200,7 @@ class MainGUI(QMainWindow):
 
     self.main_layout.addWidget(QLabel('快速選擇 :'), 0, 8, 1, 1)
     self.cmb_fast_select = QComboBox()
+    self.cmb_fast_select.addItems(list(FastSelectOptions.keys()))
     self.main_layout.addWidget(self.cmb_fast_select, 0, 9, 1, 2)
     self.btn_fast_select = QPushButton('選擇')
     self.btn_fast_select.clicked.connect(self.on_fast_select)
@@ -166,15 +210,19 @@ class MainGUI(QMainWindow):
     self.edit_log = QTextEdit(f'初始化成功; 目標網址: {TargetURI}')
     self.edit_log.setReadOnly(True)
     self.main_layout.addWidget(self.edit_log, 1, 0, 4, 12)
+    self.edit_log.moveCursor(QTextCursor.End)
     
     self.btn_save = QPushButton('選擇儲存路徑')
     self.btn_save.clicked.connect(lambda: safe_execute_func(self.execute_mainproc))
     self.main_layout.addWidget(self.btn_save, 5, 10, 1, 2)
 
-    self.chk_openfinished = QCheckBox("生成檔案後自動開啟")
+    self.chk_openfinished = QCheckBox("執行完畢後自動開啟")
     self.chk_openfinished.stateChanged.connect(self.on_auto_open)
     self.main_layout.addWidget(self.chk_openfinished, 5, 7, 1, 3)
     
+    self.on_fast_select()
+
+    self.auto_open = False
     self.active_buttons = [
       self.in_startdate, self.in_enddate, self.btn_fast_select, self.btn_save,
       self.cmb_fast_select
@@ -184,23 +232,150 @@ class MainGUI(QMainWindow):
     self.auto_open = True if signal > 0 else False
 
   def on_fast_select(self):
-    date = self.in_startdate.date()
-    date = (date.year(), date.month(), date.day())
+    current = datetime.now()
+    qdate_ed = QDate(current.year, current.month, current.day)
+    qdate_st = QDate(current.year, current.month, current.day)
+    idx = self.cmb_fast_select.currentIndex()
+    for i,key in enumerate(FastSelectOptions.keys()):
+      if i != idx:
+        continue
+      val = FastSelectOptions[key]
+      if val == 'ThisYear':
+        qdate_st = QDate(current.year, 1, 1)
+      elif val == 'LastYear':
+        qdate_st = QDate(current.year-1, 1, 1)
+        qdate_ed = QDate(current.year-1,12,31)
+      elif val == 'ThisMonth':
+        qdate_st = QDate(current.year, current.month, 1)
+      else:
+        tdelta  = timedelta(val)
+        date_st = current - tdelta
+        qdate_st = QDate(date_st.year, date_st.month, date_st.day)
+      break
+    self.in_startdate.setDate(qdate_st)
+    self.in_enddate.setDate(qdate_ed)
+    
 
   def execute_mainproc(self):
     global Signal
-    self.disable_buttons()
     self.edit_log.append("程式開始運行...")
     save_path, _ = QFileDialog.getSaveFileName(self, '另存為...', './', 'CSV UTF-8 (逗號分隔) (*.csv)')
+    if not save_path:
+      Signal = SigAbort
+      return
     self.edit_log.append(f"輸出路徑: {save_path}")
+    if not is_file_writable(save_path):
+      Signal = SigAbort
+      self.edit_log.append("程式無法輸出至指定的檔案")
+      QMessageBox.critical(None, 'Error', '無法輸出至指定的路徑, 請確認您有權限寫入且該檔案沒有被開啟!')
+      return
+
+    self.disable_buttons()
     Signal = SigWorking
+    _th = Thread(target=self.start_async, args=(save_path,))
+    _th.start()
     while Signal == SigWorking:
       MainApp.processEvents()
-      
+    
+    if Signal == SigError and LastError:
+      handle_exception(*LastError, window=MainWindow)
+    else:
+      self.edit_log.append("程式執行完畢!")
+      QMessageBox.information(self, 'Info', "程式執行完畢!", QMessageBox.Ok)
     self.enable_buttons()
 
-  def start_async(self):
-    pass
+    if self.auto_open:
+        _ok = open_external_file(save_path)
+        if not _ok:
+          self.edit_log("自動開啟不支援當前版本的作業系統, 請手動開啟")
+
+  def verify_internet(self):
+    global Signal
+    self.edit_log.moveCursor(QTextCursor.End)
+    self.edit_log.append("\n確認連線狀態...")
+    if not is_internet_available():
+      Signal = SigAbort
+      self.edit_log.append("無網路連線")
+      QMessageBox.critical(None, 'Error', '沒有網際網路連線!')
+      return False
+    else:
+      self.edit_log.append("已連線")
+    return True
+
+  def start_async(self, output_path):
+    global LastError, Signal
+    try:
+      self._start_async_proc(output_path)
+      Signal = SigComplete
+    except Exception as err:
+      err_info = traceback.format_exc()
+      LastError = (err, err_info)
+      Signal = SigError
+
+  def _start_async_proc(self, output_path):
+    global Signal
+    if not self.verify_internet():
+      print('asd')
+      return
+    print(2)
+    qdate_st = self.in_startdate.date()
+    qdate_ed = self.in_enddate.date()
+    date_st  = datetime(qdate_st.year(), qdate_st.month(), qdate_st.day())
+    date_ed  = datetime(qdate_ed.year(), qdate_ed.month(), qdate_ed.day()) + timedelta(1)
+    if date_ed < date_st:
+      date_st,date_ed = date_ed,date_st
+    page = 0
+    html = None
+    flag_outdated = False
+    try:
+      file = open(output_path, 'w')
+      file.write("標題,網址,分類\n")
+      
+      while not flag_outdated:
+        # Index processing
+        uri = f"{TargetURI}?{PageParam}={page}"
+        self.edit_log.append(f"正在連線至 {uri}")
+        try:
+          html = requests.get(uri).content
+          html = BS(html, 'html.parser')
+        except Exception:
+          Signal = SigAbort
+          self.edit_log.append(f"程式無法連線至 {uri}")
+          QMessageBox.critical(None, 'Error', f"程式無法連線至 {uri}; 請檢查網路連線正常且該網站有上線.")
+          break
+        posts = parse_content(html)
+        page += 1
+        # last page (has nothing)
+        if not posts:
+          break
+        for post in posts:
+          title = post['title']
+          date  = str2date(post['date'])
+          if date > date_ed:
+            pass
+          if date < date_st:
+            flag_outdated = True
+            break
+          print(title, date)
+          file.write(f"{title},{post['link']},")
+
+          # get link to news post and extract tags
+          self.edit_log.append(f"正在取得新聞分類 (標題: {title})")
+          try:
+            html = requests.get(post['link']).content
+            html = BS(html, 'html.parser')
+          except Exception:
+            Signal = SigAbort
+            self.edit_log.append(f"程式無法連線至 {post['link']}")
+            QMessageBox.critical(None, 'Error', f"程式無法連線至 {post['link']}; 請檢查網路連線正常且該網站有上線.")
+            break
+          tags = parse_content_tags(html)
+          file.write(" ".join(tags) + '\n')
+        # for each post
+      # while not outdated
+    finally:
+      file.close()
+        
 
   def enable_buttons(self):
     for btn in self.active_buttons:
